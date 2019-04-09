31cf7d1999f90f1f58044a8ecb8fa81ebd1e1303
Jessica Greene <jessica.greene@ecosia.org>
2019-04-09 12:39:55 +0200

    Remove references to prometheus from main.py

--- app/main.go
+++ app/main.go
@@ -3,26 +3,14 @@ package main
 import (
 	"html/template"
 	"net/http"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type response struct {
 	count int
 }
 
-var (
-	dummyCounter = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Name: "requests",
-		},
-	)
-)
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	data := response{count: 1}
-	dummyCounter.Inc()
 	t, err := template.ParseFiles("./templates/withResponse.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -37,9 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	prometheus.MustRegister(dummyCounter)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler)
-	mux.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe("0.0.0.0:8000", mux)
 }
